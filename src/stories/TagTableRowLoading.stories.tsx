7cba0334f5d3a1ea66e02dadd2165c78e1add110
Emkerix <michalekrx8@gmail.com>
2024-03-31 23:03:25 +0200

    feat: TagTableRowLoading stories

---
+++ src/stories/TagTableRowLoading.stories.tsx
@@ -0,0 +1,11 @@
+import TagTableRowLoading from "../components/TagTable/TagTableBody/TagTableRowLoading"
+
+export default {
+  component: TagTableRowLoading,
+  title: "TagTableRow/Loading ",
+  tags: ["autodocs"],
+}
+
+export const Default = {
+  args: {},
+}
