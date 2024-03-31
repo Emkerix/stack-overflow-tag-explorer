2d64c50edc2b9bc4bb1edd9c842081903e53013b
Emkerix <michalekrx8@gmail.com>
2024-03-31 22:22:57 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TagTable from "./components/TagTable/TagTable"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h2>StackOverflow Tag Explorer</h2>
+      <TagTable />
+    </>
+  )
+}
+
+export default App
