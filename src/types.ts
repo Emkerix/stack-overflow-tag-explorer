1b1e8bbe95fdf9afb4439b65749b5c2072323246
Emkerix <michalekrx8@gmail.com>
2024-03-31 22:25:19 +0200

    feat: types and styles

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export interface TagResponseError {
+  error_id: number
+  error_message: string
+  error_name: string
+}
+
+export interface TagResponseSuccess {
+  items: TagItem[]
+  has_more: boolean
+  quota_max: number
+  quota_remaining: number
+  total: number
+}
+
+export interface TagItem {
+  has_synonyms: boolean
+  is_moderator_only: boolean
+  is_required: boolean
+  count: number
+  name: string
+}
+
+export type Tag = Pick<TagItem, "count" | "name">
+
+export interface TagExplorerState {
+  page: number
+  totalPages: number
+  rowsPerPage: number
+  order: "asc" | "desc"
+  tags: Tag[]
+  quotaRemaining: number
+  isLoading: boolean
+  error: Partial<TagResponseError>
+}
+
+export type TagResponse = TagResponseSuccess | TagResponseError
